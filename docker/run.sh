6857d63a0602943f0714bd73e8a79e6fa23561b2
davidmort <davidmort100@gmail.com>
2019-08-20 14:06:24 +0100

    remove the double quotes around the size field in the manifest

--- docker/run.sh
+++ docker/run.sh
@@ -137,7 +137,7 @@ cat > "$filename".manifest <<-EOF
   "schemaVersion": 1,
   "files": [
     {
-      "sizeBytes": "$(stat -c%s $filename)",
+      "sizeBytes": $(stat -c%s $filename),
       "md5sum": "$(openssl md5 "$filename" | awk '{ print $2 }')",
       "relativePath": "./",
       "name": "$filename"
